9f5162be90f3b0c14fe4833a7f7b8877ad47e8e3
Ed Wagner <ed@edwagner.org>
2024-04-07 15:40:18 -0500

    Reformatted with black

--- src/ofxstatement_schwab_json/plugin.py
+++ src/ofxstatement_schwab_json/plugin.py
@@ -9,9 +9,11 @@ from ofxstatement.parser import AbstractStatementParser
 from ofxstatement.statement import Statement, InvestStatementLine, StatementLine
 
 import logging
+
 LOGGER = logging.getLogger(__name__)
 
 import json
+
 
 class SchwabJsonPlugin(Plugin):
     """Parses Schwab JSON export of investment transactions"""
@@ -28,7 +30,7 @@ class SchwabJsonParser(AbstractStatementParser):
         self.filename = filename
         self.statement = Statement()
         self.statement.broker_id = "Schwab"
-        match = re.search(r'(.*)_Transactions_.*\.json', path.basename(filename))
+        match = re.search(r"(.*)_Transactions_.*\.json", path.basename(filename))
         if match:
             self.statement.account_id = match[1]
         self.id_generator = IdGenerator()
@@ -37,81 +39,92 @@ class SchwabJsonParser(AbstractStatementParser):
         """Main entry point for parsers"""
         with open(self.filename, "r") as f:
             # Reverse the lines so that they are in chronological order
-            self.import_lines(reversed(json.load(f)['BrokerageTransactions']))
+            self.import_lines(reversed(json.load(f)["BrokerageTransactions"]))
             return self.statement
 
     def import_lines(self, transactions):
         for tran in transactions:
-            date = datetime.strptime(tran["Date"][0:10], '%m/%d/%Y')
+            date = datetime.strptime(tran["Date"][0:10], "%m/%d/%Y")
             id = self.id_generator.create_id(date)
             line = InvestStatementLine(
-                id=id,
-                date=date,
-                memo=f'{tran["Action"]} {tran["Description"]}')
+                id=id, date=date, memo=f'{tran["Action"]} {tran["Description"]}'
+            )
             if len(tran["Amount"]) > 0:
-                line.amount = Decimal(re.sub("[$,]","",tran["Amount"]))
+                line.amount = Decimal(re.sub("[$,]", "", tran["Amount"]))
 
             action = tran["Action"]
             if action == "Sell":
                 line.trntype = "SELLSTOCK"
                 line.trntype_detailed = "SELL"
-                line.security_id=tran["Symbol"]
-                line.units = Decimal("-" + re.sub("[,-]","",tran["Quantity"])) # Ensure a negative number
-                line.unit_price = Decimal(re.sub("[$,]","",tran["Price"]))
+                line.security_id = tran["Symbol"]
+                line.units = Decimal(
+                    "-" + re.sub("[,-]", "", tran["Quantity"])
+                )  # Ensure a negative number
+                line.unit_price = Decimal(re.sub("[$,]", "", tran["Price"]))
                 if len(tran["Fees & Comm"]) > 0:
-                    line.fees = Decimal(re.sub("[$,]","",tran["Fees & Comm"]))
-            elif (action == "Cash Dividend"
-                  or action == "Qualified Dividend"
-                  or action == "Non-Qualified Div"
-                  or action == "Pr Yr Non Qual Div"
-                  or action == "Pr Yr Non-Qual Div"
-                  or action == "Qual Div Reinvest"
-                  or action == "Reinvest Dividend"):
+                    line.fees = Decimal(re.sub("[$,]", "", tran["Fees & Comm"]))
+            elif (
+                action == "Cash Dividend"
+                or action == "Qualified Dividend"
+                or action == "Non-Qualified Div"
+                or action == "Pr Yr Non Qual Div"
+                or action == "Pr Yr Non-Qual Div"
+                or action == "Qual Div Reinvest"
+                or action == "Reinvest Dividend"
+            ):
                 line.trntype = "INCOME"
                 line.trntype_detailed = "DIV"
-                line.security_id=tran["Symbol"]
+                line.security_id = tran["Symbol"]
             elif action == "Long Term Cap Gain":
                 line.trntype = "INCOME"
                 line.trntype_detailed = "CGLONG"
-                line.security_id=tran["Symbol"]
+                line.security_id = tran["Symbol"]
             elif action == "Short Term Cap Gain":
                 line.trntype = "INCOME"
                 line.trntype_detailed = "CGSHORT"
-                line.security_id=tran["Symbol"]
+                line.security_id = tran["Symbol"]
             elif action == "Bank Interest" and len(tran["Symbol"]) > 0:
                 line.trntype = "INCOME"
                 line.trntype_detailed = "INTEREST"
-                line.security_id=tran["Symbol"]
+                line.security_id = tran["Symbol"]
             elif action == "Buy" or action == "Reinvest Shares":
                 line.trntype = "BUYSTOCK"
                 line.trntype_detailed = "BUY"
-                line.security_id=tran["Symbol"]
-                line.units = Decimal(re.sub("[,]","",tran["Quantity"]))
-                line.unit_price = Decimal(re.sub("[$,]","",tran["Price"]))
+                line.security_id = tran["Symbol"]
+                line.units = Decimal(re.sub("[,]", "", tran["Quantity"]))
+                line.unit_price = Decimal(re.sub("[$,]", "", tran["Price"]))
                 if len(tran["Fees & Comm"]) > 0:
-                    line.fees = Decimal(re.sub("[$,]","",tran["Fees & Comm"]))
-            elif len(tran["Symbol"]) > 0 and (action == "Journaled Shares" or action == "Spin-off"):
+                    line.fees = Decimal(re.sub("[$,]", "", tran["Fees & Comm"]))
+            elif len(tran["Symbol"]) > 0 and (
+                action == "Journaled Shares" or action == "Spin-off"
+            ):
                 line.trntype = "TRANSFER"
-                line.security_id=tran["Symbol"]
-                line.units = Decimal(re.sub("[,]","",tran["Quantity"]))
+                line.security_id = tran["Symbol"]
+                line.units = Decimal(re.sub("[,]", "", tran["Quantity"]))
                 if len(tran["Price"]) > 0:
-                    line.unit_price = Decimal(re.sub("[$,]","",tran["Price"]))
+                    line.unit_price = Decimal(re.sub("[$,]", "", tran["Price"]))
                 else:
                     line.unit_price = Decimal(0)
                 if line.amount is None:
                     line.amount = Decimal(0)
                 if action == "Spin-off":
-                    LOGGER.warning(f'You will probably want to allocate some cost basis for the {line.security_id} spin-off.')
+                    LOGGER.warning(
+                        f"You will probably want to allocate some cost basis for the {line.security_id} spin-off."
+                    )
             elif len(tran["Symbol"]) == 0 or action == "Cash In Lieu":
                 line.trntype = "INVBANKTRAN"
-                if (action == "Bank Interest"
-                        or action == "Bond Interest"
-                        or action == "Credit Interest"):
+                if (
+                    action == "Bank Interest"
+                    or action == "Bond Interest"
+                    or action == "Credit Interest"
+                ):
                     line.trntype_detailed = "INT"
-                elif (action == "MoneyLink Transfer"
-                      or action == "Internal Transfer"
-                      or action == "Journal"
-                      or action == "Journaled Shares"):
+                elif (
+                    action == "MoneyLink Transfer"
+                    or action == "Internal Transfer"
+                    or action == "Journal"
+                    or action == "Journaled Shares"
+                ):
                     line.trntype_detailed = "XFER"
                 elif action == "Wire Sent":
                     line.trntype_detailed = "DEBIT"
@@ -128,9 +141,10 @@ class SchwabJsonParser(AbstractStatementParser):
             line.assert_valid()
             self.statement.invest_lines.append(line)
 
+
 class IdGenerator:
     """Generates a unique ID based on the date
-    
+
     Hopefully any JSON file that we get will have all the transactions for a
     given date, and hopefully in the same order each time so that these IDs
     will match up across exports.
--- tests/test_statement.py
+++ tests/test_statement.py
@@ -7,7 +7,9 @@ from decimal import Decimal
 from ofxstatement_schwab_json.plugin import SchwabJsonPlugin
 
 import logging
+
 LOGGER = logging.getLogger(__name__)
+
 
 @pytest.fixture
 def statement() -> ofxstatement.statement.Statement:
@@ -18,23 +20,28 @@ def statement() -> ofxstatement.statement.Statement:
     parser = plugin.get_parser(test_filename)
     return parser.parse()
 
+
 def test_parsing(statement):
     assert statement is not None
     assert len(statement.invest_lines) == 16
 
+
 def test_ids(statement):
     assert statement.invest_lines[0].id == "20230922-1"
     assert statement.invest_lines[1].id == "20230922-2"
+
 
 def test_transfer(statement):
     line = next(x for x in statement.invest_lines if x.id == "20240212-1")
     assert line.trntype == "INVBANKTRAN"
     assert line.trntype_detailed == "XFER"
 
+
 def test_journal_cash(statement):
     line = next(x for x in statement.invest_lines if x.id == "20240207-1")
     assert line.trntype == "INVBANKTRAN"
     assert line.trntype_detailed == "XFER"
+
 
 def test_journal_security(statement):
     line = next(x for x in statement.invest_lines if x.id == "20240208-1")
@@ -44,32 +51,38 @@ def test_journal_security(statement):
     assert line.amount == 0
     assert line.unit_price == 1
 
+
 def test_sell(statement):
     line = next(x for x in statement.invest_lines if x.id == "20240209-1")
     assert line.trntype == "SELLSTOCK"
     assert line.trntype_detailed == "SELL"
     assert line.units == -1000
     assert line.amount == 1000
-    
+
+
 def test_interest(statement):
     line = next(x for x in statement.invest_lines if x.id == "20240116-1")
     assert line.trntype == "INVBANKTRAN"
     assert line.trntype_detailed == "INT"
+
 
 def test_wire(statement):
     line = next(x for x in statement.invest_lines if x.id == "20231212-3")
     assert line.trntype == "INVBANKTRAN"
     assert line.trntype_detailed == "DEBIT"
 
+
 def test_bank_fee(statement):
     line = next(x for x in statement.invest_lines if x.id == "20231212-1")
     assert line.trntype == "INVBANKTRAN"
     assert line.trntype_detailed == "SRVCHG"
 
+
 def test_bank_misc(statement):
     line = next(x for x in statement.invest_lines if x.id == "20231212-2")
     assert line.trntype == "INVBANKTRAN"
     assert line.trntype_detailed == "OTHER"
+
 
 def test_spin_off(statement):
     line = next(x for x in statement.invest_lines if x.id == "20240401-1")
@@ -79,8 +92,9 @@ def test_spin_off(statement):
     assert line.amount == 0
     assert line.unit_price == 0
 
+
 def test_cash_in_lieu(statement):
     line = next(x for x in statement.invest_lines if x.id == "20240402-1")
     assert line.trntype == "INVBANKTRAN"
     assert line.trntype_detailed == "CREDIT"
-    assert line.amount == Decimal('32.68')
+    assert line.amount == Decimal("32.68")
